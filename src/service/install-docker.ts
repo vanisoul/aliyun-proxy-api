c77daed0d7a387824296f1a03fc8a690ab08ea98
vanisoul <ga2006088445@gmail.com>
2023-11-06 22:53:01 +0800

    Feat : 修正 docker 鏡像位置

--- src/service/install-docker.ts
+++ src/service/install-docker.ts
@@ -8,7 +8,7 @@ export const installDockerStep = [
   "sudo rm -rf /etc/apt/keyrings/docker.gpg",
   "curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --batch --dearmor -o /etc/apt/keyrings/docker.gpg",
   "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
-  `echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null`,
+  `echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] http://mirrors.aliyun.com/docker-ce/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null`,
   "sudo apt-get update",
   "sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
 ];
